9c6aa35e3f47137a3581621a6f770c10d3c924a9
kartikeyametikoti <149247879+kartikeyametikoti@users.noreply.github.com>
2025-03-18 16:39:47 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -31,7 +31,7 @@ class User(db.Model):
 with app.app_context():
     db.create_all()
 
-# **Root Route ("/")**
+# **Root Route ("/api")**
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({"message": "This is my backend application"}), 200
