84222b11362f53cb32b765a8ab9bd3b0607f0382
kartikeyametikoti <149247879+kartikeyametikoti@users.noreply.github.com>
2025-03-19 10:36:49 +0530

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -14,7 +14,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://${{ secrets.BACKEND_URL }}/api/login", formData);
+      const response = await axios.post("http://${{ secrets.INGRESS_BACKEND_URL }}/api/login", formData);
       alert(response.data.message);
     } catch (error) {
       alert(error.response.data.message);
