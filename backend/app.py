ed6321026aa21a46ac37f4d249dd7c2be26fd347
kartikeyametikoti <149247879+kartikeyametikoti@users.noreply.github.com>
2025-03-20 12:36:59 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -34,7 +34,7 @@ with app.app_context():
 # **Root Route ("/api")**
 @app.route('/api', methods=['GET'])
 def home():
-    return jsonify({"message": "This is my backend application now"}), 200
+    return jsonify({"message": "This is my backend application now "}), 200
 
 # **Signup Route**
 @app.route('/api/signup', methods=['POST'])
